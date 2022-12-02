aebb74b0f630f08db20361f1434555f243579389
emilmarcel <55388306+marcelcph@users.noreply.github.com>
2022-12-02 21:55:58 +0100

    Create heroheader.component.ts

---
+++ src/app/homepagecomponents/heroheader/heroheader.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroheader',
+  templateUrl: './heroheader.component.html',
+  styleUrls: ['./heroheader.component.scss'],
+})
+export class HeroheaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
