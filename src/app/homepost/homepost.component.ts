0ceba944a88185096124f58677811dd627cfb69f
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2022-12-02 21:35:56 +0100

    5

---
+++ src/app/homepost/homepost.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepost',
+  templateUrl: './homepost.component.html',
+  styleUrls: ['./homepost.component.scss'],
+})
+export class HomepostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
