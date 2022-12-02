32d63574fd5d4ffeef88ef6bb20a741c712dde75
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2022-12-02 21:36:03 +0100

    7

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { HomepostComponent } from '../homepost/homepost.component';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
@@ -13,8 +13,8 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page, HomepostComponent]
 })
 export class Tab1PageModule {}
