07f45a76174bb7e743aa96c86326b07f0fb6a102
emilmarcel <55388306+marcelcph@users.noreply.github.com>
2022-12-03 21:26:18 +0100

    tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gs-primary': '#35A94F',
+      }
+    },
   },
   plugins: [],
-}
+};
