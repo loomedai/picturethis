d5ae194ccc5054ae2984ddab75b29b43411732f1
loomedai <bbrith@hotmail.com>
2022-12-06 00:48:24 +0100

    create works

--- src/app/createpost/createpost.component.ts
+++ src/app/createpost/createpost.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiService} from '../api/api.service'; 
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-createpost',
@@ -8,10 +11,35 @@ import {ApiService} from '../api/api.service';
 })
 export class CreatepostComponent implements OnInit {
 
-  constructor(private service:ApiService) { }
+  constructor(private service:ApiService, alertcontroller:AlertController) { }
+
+  errormsg:any;
+  successmsg:any;
+ // const [presentAlert] = useIonAlert();
 
   ngOnInit(): void {
-    this.service.getAllData().subscribe((res)=>{
-  });
+ //   this.service.getAllData().subscribe((res)=>{
+ // });
  }
+
+postForm = new FormGroup({
+  'title':new FormControl('',Validators.required),
+  'img':new FormControl('',Validators.required),
+  'description': new FormControl('',Validators.required)
+});
+
+postSubmit(){
+    if(this.postForm.valid){
+      
+      this.service.CreateData(this.postForm.value).subscribe((res)=>{
+       console.log(res, 'res==>'); 
+       this.postForm.reset()
+      });
+      
+    }
+    else{
+     this.errormsg = 'All fields must be filled'
+    }
+}
+
 }
