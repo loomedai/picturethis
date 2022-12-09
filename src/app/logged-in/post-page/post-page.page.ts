eec97083d206d31119837f7409513dad87bbb391
emilmarcel <55388306+marcelcph@users.noreply.github.com>
2022-12-09 16:52:20 +0100

    Post page

--- src/app/logged-in/post-page/post-page.page.ts
+++ src/app/logged-in/post-page/post-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api/api.service';
 
 @Component({
   selector: 'app-post-page',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostPagePage implements OnInit {
 
-  constructor() { }
+  constructor(private service:ApiService) { }
 
-  ngOnInit() {
+  readData:any;
+
+  ngOnInit(): void {
+    this.service.getAllData().subscribe((res)=>{
+      console.log(res,"res==>");
+      this.readData = res.data;
+    });
   }
+  counter = 0;
 
+  increment() {
+    this.counter++;
+  }
 }
