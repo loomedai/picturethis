8735e689b1ae8a05b9132ef5c799be09b32a0f7d
emilmarcel <55388306+marcelcph@users.noreply.github.com>
2022-12-19 13:31:01 +0100

    Fjernet leaderboardet + margins

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -6,7 +6,6 @@ import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { HomepostComponent } from '../homepagecomponents/homepost/homepost.component';
 import { HeroheaderComponent } from '../homepagecomponents/heroheader/heroheader.component';
-import { LeaderboardComponent } from '../homepagecomponents/leaderboard/leaderboard.component';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 import { FooterComponent } from '../homepagecomponents/footer/footer.component';
 
@@ -20,6 +19,6 @@ import { FooterComponent } from '../homepagecomponents/footer/footer.component';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule,
   ],
-  declarations: [Tab1Page,  HomepostComponent, HeroheaderComponent, LeaderboardComponent, FooterComponent]
+  declarations: [Tab1Page,  HomepostComponent, HeroheaderComponent, FooterComponent]
 })
 export class Tab1PageModule {}
